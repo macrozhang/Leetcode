44bc46788213b528987e676ab4806c66c79d6212
JamesMacroZhang <33259827+macrozhang@users.noreply.github.com>
2020-03-05 00:46:54 +0100

    # 1103. 分糖果 II

---
+++ 20200304_leetcode_1103.py
@@ -0,0 +1,22 @@
+# 1103. 分糖果 II
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def distributeCandies(candies: int, num_people: int) -> List[int]:
+        ans = [0] * num_people
+        i = 0
+        while candies > i:
+            ans[i % num_people] += i + 1
+            print("第{}次：{}".format(i+1, ans))
+            candies -= i + 1  # candies = candies - (i + 1)
+            i += 1
+            # ans[i % num_people] += candies
+            # print("结果：{}".format(ans))
+        ans[i % num_people] += candies
+        print("结果：{}".format(ans))
+
+
+test = Solution()
+test.distributeCandies(10, 3)
