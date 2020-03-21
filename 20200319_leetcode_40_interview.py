f3bc23591d1dc7c94f43e5bf086f1018b21e8607
JamesMacroZhang <33259827+macrozhang@users.noreply.github.com>
2020-03-22 00:26:31 +0100

    # 面试题40. 最小的k个数

---
+++ 20200319_leetcode_40_interview.py
@@ -0,0 +1,18 @@
+# 面试题40. 最小的k个数
+
+
+class Solution:
+    @staticmethod
+    def getLeastNumbers(arr: list, k: int):
+        # 布隆过滤器原理，因为知道数组的最大值
+        nums = [0] * 10000  # 申请一个包含最大值个数的元素
+        for a in arr:
+            nums[a] += 1  # 对重复元素计数
+        output = []
+        i = 0  # 从0开始遍历（从最小值开始遍历）
+        while len(output) < k:
+            if nums[i] >= 1:  # 如果该索引处的值大于1，则说明该值存在至少1次，循环往output里面写
+                for j in range(nums[i]):
+                    output.append(i)
+            i += 1
+        return output[:k]  # 一定要注意单个重复的元素多余k个的情况
