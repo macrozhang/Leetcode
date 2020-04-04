f24425581b818b9a5e8c42a8732cc64a780a446d
JamesMacroZhang <33259827+macrozhang@users.noreply.github.com>
2020-04-04 22:01:35 +0200

    # 460. LFU缓存

---
+++ 20200403_leetcode_460.py
@@ -0,0 +1,107 @@
+# 460. LFU缓存
+
+
+# class LFUCache:
+
+# def __init__(self, capacity: int):
+# def get(self, key: int) -> int:
+# def put(self, key: int, value: int) -> None:
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+"""
+最不经常使用算法（LFU）：
+这个缓存算法使用一个计数器来记录条目被访问的频率。
+通过使用LFU缓存算法，最低访问数的条目首先被移除。
+这个方法并不经常使用，因为它无法对一个拥有最初高访问率之后长时间没有被访问的条目缓存负责。
+"""
+
+import collections
+
+
+class Node:
+    def __init__(self, key, val, pre=None, nex=None, freq=0):
+        self.pre = pre
+        self.nex = nex
+        self.freq = freq
+        self.val = val
+        self.key = key
+
+    def insert(self, nex):
+        nex.pre = self
+        nex.nex = self.nex
+        self.nex.pre = nex
+        self.nex = nex
+
+
+def create_linked_list():
+    head = Node(0, 0)
+    tail = Node(0, 0)
+    head.nex = tail
+    tail.pre = head
+    return (head, tail)
+
+
+class LFUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self.minFreq = 0
+        self.freqMap = collections.defaultdict(create_linked_list)
+        self.keyMap = {}
+
+    def delete(self, node):
+        if node.pre:
+            node.pre.nex = node.nex
+            node.nex.pre = node.pre
+            if node.pre is self.freqMap[node.freq][0] and node.nex is self.freqMap[node.freq][-1]:
+                self.freqMap.pop(node.freq)
+        return node.key
+
+    def increase(self, node):
+        node.freq += 1
+        self.delete(node)
+        self.freqMap[node.freq][-1].pre.insert(node)
+        if node.freq == 1:
+            self.minFreq = 1
+        elif self.minFreq == node.freq - 1:
+            head, tail = self.freqMap[node.freq - 1]
+            if head.nex is tail:
+                self.minFreq = node.freq
+
+    def get(self, key: int) -> int:
+        if key in self.keyMap:
+            self.increase(self.keyMap[key])
+            return self.keyMap[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity != 0:
+            if key in self.keyMap:
+                node = self.keyMap[key]
+                node.val = value
+            else:
+                node = Node(key, value)
+                self.keyMap[key] = node
+                self.size += 1
+            if self.size > self.capacity:
+                self.size -= 1
+                deleted = self.delete(self.freqMap[self.minFreq][0].nex)
+                self.keyMap.pop(deleted)
+            self.increase(node)
+
+
+cache = LFUCache(2)             # capacity (缓存容量)
+cache.put(1, 1)
+cache.put(2, 2)
+print(cache.get(1))             # 返回1
+cache.put(3, 3)                 # 去除 key 2
+print(cache.get(2))             # 返回 -1 (未找到key 2)
+print(cache.get(3))             # 返回 3
+cache.put(4, 4)                 # 去除 key 1
+print(cache.get(1))             # 返回 -1 (未找到 key 1)
+print(cache.get(3))             # 返回 3
+print(cache.get(4))             # 返回 4
