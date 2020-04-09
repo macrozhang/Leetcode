8944b961a2d8b51c064d290c32acfba5a3523d0e
JamesMacroZhang <33259827+macrozhang@users.noreply.github.com>
2020-04-09 20:49:31 +0200

    # 22. 括号生成

---
+++ 20200409_leetcode_22.py
@@ -0,0 +1,36 @@
+# 22. 括号生成
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def generate(A):
+            if len(A) == 2*n:
+                if valid(A):
+                    ans.append("".join(A))
+            else:
+                A.append('(')
+                generate(A)
+                A.pop()
+                A.append(')')
+                generate(A)
+                A.pop()
+
+        def valid(A):
+            bal = 0
+            for c in A:
+                if c == '(':
+                    bal += 1
+                else:
+                    bal -= 1
+                if bal < 0:
+                    return False
+            return bal == 0
+
+        ans = []
+        generate([])
+        return ans
+
+
+test = Solution()
+print(test.generateParenthesis(3))
